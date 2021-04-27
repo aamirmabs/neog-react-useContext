365725efe2fefddaa70a6dfa1e6b0e098131b7f2
aamyear <aamyear@gmail.com>
2021-04-27 05:02:42 +0400

    ex02 setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 
-function App() {
+export function ProductListing() {
+  return [1, 2, 3, 4, 5].map((item) => <h3>Product {item}</h3>);
+}
+
+export function Cart() {
+  return <h2>Items in cart: </h2>;
+}
+
+export default function App() {
   return (
     <div className="App">
-      <h1>neoG useContext Lesson</h1>
+      <h1>eCommerce</h1>
+      <Cart />
+      <ProductListing />
     </div>
   );
 }
-
-export default App;
