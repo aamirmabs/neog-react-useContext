9f174113adf4992dafc64fb3b24f5fd2eea7764f
aamyear <aamyear@gmail.com>
2021-04-28 07:08:38 +0400

    ex03 create and provide context

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { CartContext } from "./contexts";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CartContext.Provider value={{ cartItems: 4 }}>
+      <App />
+    </CartContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
